9636015c72a02bb133d5dc6e559af61ec721a520
asthaagarwal01 <astha2agarwal5@gmail.com>
2023-12-17 20:00:03 +0530

    database fields

--- models/registeration.go
+++ models/registeration.go
@@ -1,10 +1,9 @@
 package models
 
-import "gorm.io/gorm"
-
-type Registration struct {
-    gorm.Model
-    StudentID  uint
-    SubjectID  uint
-    Status     string // "requested", "approved", "registered"
+type RegistrationDetail struct {
+    Name        string `json:"name" gorm:"column:name"`
+    RollNo      int    `json:"rollNo" gorm:"column:roll_no;primaryKey"`
+    SubjectName string `json:"subjectName" gorm:"column:subject_name"`
+    Status      string `json:"status" gorm:"column:status"`
+    SRNo        int    `json:"srNo" gorm:"column:SR_No;autoIncrement"`
 }
